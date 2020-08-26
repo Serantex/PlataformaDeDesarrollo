2e1d19801b90e9c280133f601d76eecebf4e331e
lautaro serantes <lautaro.serantes@davinci.edu.ar>
2020-08-25 23:00:50 -0300

    ejercicio1

---
+++ 25082020/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intentos = 0;
+            string respuesta = "";
+            
+            while(respuesta!="calecita" && intentos<3)
+            {
+                Console.Write("Vas en un avion. Delante tienes un caballo y detras un Camion. ¿Donde estas?");
+
+                respuesta = Console.ReadLine();
+
+                intentos++;
+
+                if (respuesta != "calecita")
+                {
+                    if (intentos == 3)
+                    {
+                        Console.Write("PERDISTE");
+                        break;
+                    }
+                    Console.Write("ups intenta otra vez\n");
+                }
+                else
+                {
+                    Console.Write("correcto");
+                    break;
+                }
+
+ 
+
+
+            }
+
+
+        }
+    }
+}
