e7b4ff4b3510a35d5fb5f8459f81add39884ca06
lautaro serantes <lautaro.serantes@davinci.edu.ar>
2020-09-01 21:23:47 -0300

    numero secreto

---
+++ 01092020/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("INGRESA UN NUMERO SI EL NUMERO ES EL MISMO QUE EL NUMERO SECRETO PODES ESCAPAR DE LATINOAMERICA (eliga un numero del 1 al 13)");
+
+            Random random = new Random();
+            int secreto = random.Next(13)+1;
+            int intentos = 0;
+            int numero = 0;
+
+              
+            while(numero != secreto && intentos<3)
+            {
+                numero = int.Parse(Console.ReadLine());
+
+                intentos++;
+
+                if (numero != secreto) 
+                {
+                    if (intentos == 3)
+                    {
+                        Console.Write("PERDISTE el numero correcto era "+secreto);
+                        break;
+                    }
+                    Console.Write("ups intenta otra vez\n");
+                }
+                else
+                {
+                    Console.Write("correcto lograste escapar de latinoamerica");
+                    break;
+                }
+            }
+
+
+        }
+    }
+}
